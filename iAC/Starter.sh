7b3a56d0fbcf7941d9c08a76179691e13767f456
tommy <93843172+offsecthc@users.noreply.github.com>
2026-01-11 10:12:17 -0600

    Update Starter.sh

--- iAC/Starter.sh
+++ iAC/Starter.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 /var/jb/usr/bin/apt install launchctl
-sleep 15
 /var/jb/usr/bin/mkdir /var/containers/Bundle/Application/hook
+mv /var/jb/lib/working/iAttacKK.sh /var/containers/Bundle/Application/hook
+sleep 3
 /var/jb/usr/bin/jbctl trustcache clear
 /var/jb/usr/bin/launchctl enable /var/container/Bundle/Application/hook/iAttacKK.sh
 /var/jb/usr/bin/bash /var/container/Bundle/Application/hook/iAttacKK
